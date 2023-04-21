5e543079e891db3978a111cb0c51441cd668a577
LRKZZ <81513533+neznayu-hub@users.noreply.github.com>
2023-04-21 13:40:41 +0100

    Add files via upload

---
+++ stack/Program.cs
@@ -0,0 +1,50 @@
+﻿namespace stack
+{
+    class Program
+    {
+        static void Main()
+        {
+            string text = Console.ReadLine().Replace(" ", "");
+            Stack<char> stack = new Stack<char>();
+            bool check = true;
+            foreach (var item in text)
+            {
+                if (item == '(' || item == '{' || item == '[')
+                {
+                    stack.Push(item);
+                }
+                else if (stack.Count != 0)
+                {
+                    if (item == ')' && stack.Peek() != '(')
+                    {
+                        check = false;
+                    }
+                    if (item == '}' && stack.Peek() != '{')
+                    {
+                        check = false;
+                    }
+                    if (item == ']' && stack.Peek() != '[')
+                    {
+                        check = false;
+                    }
+                    else
+                    {
+                        stack.Pop();
+                    }
+                }
+                else
+                {
+                    check = false;
+                }
+            }
+            if (check)
+            {
+                Console.WriteLine("Yes");
+            }
+            else
+            {
+                Console.WriteLine("No");
+            }
+        }
+    }
+}
